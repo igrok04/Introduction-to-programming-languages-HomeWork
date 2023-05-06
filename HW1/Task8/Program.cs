df9238bf832b6488aaf10efb2cb3469eadef94a9
igrok04 <97518422+igrok04@users.noreply.github.com>
2023-05-06 22:29:09 +0300

    Add files via upload

---
+++ HW1/Task8/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+using System;
+Console.Clear();
+Console.Write("Введите число больше или равно 1: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int B = 1;
+Console.Write($"Четные числа от 1 до {N}: ");
+while (B <= N)
+{
+    if (N == 1)
+    {
+        Console.Write("отсутствуют");
+    }
+    if (B % 2 == 0)
+    {
+        Console.Write($"{B} ");
+    }
+    B++;
+}
+
+
