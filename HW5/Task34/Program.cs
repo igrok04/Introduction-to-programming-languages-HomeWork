5bf9fa50b86cb827bd4935c5dddee2db4a6d3e09
igrok04 <97518422+igrok04@users.noreply.github.com>
2023-05-09 01:12:03 +0300

    Add files via upload

---
+++ HW5/Task34/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2
+*/
+
+using System;
+using static System.Console;//использование функций класса консоль без вызова самого класса
+
+Clear();
+
+int[] array = GetArray(4, 100, 1000);
+WriteLine($"Массив: [{String.Join(", ", array)}]");
+//int countElements = GetCountElements(array, 100, 999);
+WriteLine($"Количество чётных чисел в массиве = {GetCountElements(array, 100, 999)}");
+
+
+int GetCountElements(int[] inArray, int leftRange, int rigthRange)//метод, который вернет количество элементов, 
+//находящихся в заданном диапазоне
+{
+    int count = 0;
+    foreach (int item in inArray)
+    {
+        if (item % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] GetArray(int size, int min, int max)//метод, формирующий массив
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(min, max + 1);
+    }
+    return result;
+}
---
+++ HW5/Task36/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами.
+Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0
+*/
+
+using System;
+using static System.Console;//использование функций класса консоль без вызова самого класса
+
+Clear();
+
+Write($"Введите длину массива: ");
+int ElementNumbers = Convert.ToInt32(ReadLine()); 
+//int randomNumber =  RandomNumbers(ElementNumbers, 0, 100);
+WriteLine($"\nСумма элементов, стоящих на нечётных позициях: {RandomNumbers(ElementNumbers, 0, 100)}");
+
+
+
+int RandomNumbers(int ElementNumbers, int min, int max)
+  {
+  int[] randomNumber = new int[ElementNumbers];
+  int sumElements = 0;
+  Write("Получившийся массив: ");
+    for (int i = 0; i <randomNumber.Length; i++ ){
+      randomNumber[i] = new Random().Next(min, max);
+
+      Write(randomNumber[i] + ", ");
+
+      if (i % 2 != 1)
+      {
+        sumElements = sumElements + randomNumber[i];
+      }
+    }
+  return sumElements;
+  }
