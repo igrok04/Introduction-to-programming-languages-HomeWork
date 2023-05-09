e2b0a7ac6ace0311f77970077f85ef46d31db9dd
igrok04 <97518422+igrok04@users.noreply.github.com>
2023-05-09 23:25:02 +0300

    Add files via upload

---
+++ HW6/Task41/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+
+using System;
+using static System.Console;
+
+Clear();
+int[] array = askNumbers();
+int count = countAboveZero(array);
+WriteLine($"{string.Join(", ", array)} -> {count}");
+
+int[] askNumbers() {
+    Write("Введите числа через пробел: ");
+    return ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+}
+
+int countAboveZero(int[] array) {
+    return array.Where(x => x > 0).Count();
+}
---
+++ HW6/Task43/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2
+ 
+* x + b2; значения b1, k1, b2 и k2 задаются пользователем. 
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+*/
+using System;
+using static System.Console;
+
+Clear();
+WriteLine("y = k1 * x + b1");
+WriteLine("y = k2 * x + b2");
+printResult(askNumbers());
+
+int[] askNumbers() {
+    Write("Введите b1, k1, b2, k2 по порядку через пробел: ");
+    return ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+}
+
+void printResult(int[] array) {
+    double x = ((double) array[2] - array[0]) / ((double) array[1] - array[3]);
+    double y = array[3] * x + array[2];
+    WriteLine($"b1 = {array[0]}, k1 = {array[1]}, b2 = {array[2]}, k2 = {array[3]} -> ({x}; {y})");
+}
